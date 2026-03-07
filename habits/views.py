84b452257dd8bd4cdf8826a0f988f077578c06a5
slav <vrazumihindima@gmail.com>
2026-03-07 20:42:30 +0300

    habits/views.py

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Habit
+from .paginations import HabitPagination
+from .permissions import IsOwner
+from .serializers import HabitSerializer
 
-# Create your views here.
+
+class HabitListCreateView(generics.ListCreateAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = HabitPagination
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class HabitRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user)
+
+
+class PublicHabitListView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [permissions.AllowAny]
+    pagination_class = HabitPagination
+    queryset = Habit.objects.filter(is_public=True)
