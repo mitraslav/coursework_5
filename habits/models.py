0b4d95b5e978ca6088278be7a34eb320f2fabff2
slav <vrazumihindima@gmail.com>
2026-03-07 20:39:40 +0300

    Habit model created

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,55 @@
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Habit(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='habits',
+        verbose_name='Пользователь'
+    )
+    place = models.CharField(max_length=255, verbose_name='Место')
+    time = models.TimeField(verbose_name='Время')
+    action = models.CharField(max_length=255, verbose_name='Действие')
+
+    is_pleasant = models.BooleanField(
+        default=False,
+        verbose_name='Признак приятной привычки'
+    )
+    related_habit = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='related_for',
+        verbose_name='Связанная привычка'
+    )
+    periodicity = models.PositiveSmallIntegerField(
+        default=1,
+        validators=[MinValueValidator(1), MaxValueValidator(7)],
+        verbose_name='Периодичность в днях'
+    )
+    reward = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        verbose_name='Вознаграждение'
+    )
+    execution_time = models.PositiveIntegerField(
+        validators=[MaxValueValidator(120)],
+        verbose_name='Время выполнения в секундах'
+    )
+    is_public = models.BooleanField(
+        default=False,
+        verbose_name='Признак публичности'
+    )
+
+    class Meta:
+        verbose_name = 'Привычка'
+        verbose_name_plural = 'Привычки'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.action
