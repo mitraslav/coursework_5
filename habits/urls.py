43bad83acbaa10a5a090e54707df909e5c6c2e6c
slav <vrazumihindima@gmail.com>
2026-03-07 20:45:33 +0300

    habits/urls.py

---
+++ habits/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import HabitListCreateView, HabitRetrieveUpdateDestroyView, PublicHabitListView
+
+urlpatterns = [
+    path('', HabitListCreateView.as_view(), name='habit-list-create'),
+    path('public/', PublicHabitListView.as_view(), name='public-habits'),
+    path('<int:pk>/', HabitRetrieveUpdateDestroyView.as_view(), name='habit-detail'),
+]
